a9f0d1b3beab2a9733eac60c34cb405c10490957
Adykey79 <adykeydev@gmail.com>
2024-03-31 14:04:53 -0700

    Initial Commit

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 // The url with data
-const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json"
+const url = "../../samples.json"
 
 // Display the default plots
 function init() {
